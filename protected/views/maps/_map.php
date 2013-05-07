83a4a27a18e841579facf957778570922602f7bf
Lorenzo Pini <lore.gnafu@gmail.com>
2013-05-07 15:36:34 +0200

    html encoding
    Legenda della presisoni minime

--- protected/views/maps/_map.php
+++ protected/views/maps/_map.php
@@ -553,12 +553,12 @@ map.addLayer(layer_<?php echo $l['name']; ?>);
     <div id="map" class="cols">
 		<div id="resize_map"></div>
 		<div id="pressure_legend">
-			<div class="p_legend" alt="" title=""><span id="p4" style="background:#c4c4c4;"></span>P < 4</div>
-			<div class="p_legend" alt="" title=""><span id="4p8" style="background:#fa4f47;"></span>4 < P < 8</div>
-			<div class="p_legend" alt="" title=""><span id="8p15" style="background:#fb8a3d;"></span>8 < P < 15</div>
-			<div class="p_legend" alt="" title=""><span id="15p25" style="background:#91ff00;"></span>15 < P < 25</div>
-			<div class="p_legend" alt="" title=""><span id="25p50" style="background:#60feff;"></span>25 < P < 50</div>
-			<div class="p_legend" alt="" title=""><span id="p50" style="background:#4b44ff;"></span>P > 50</div>
+			<div class="p_legend" alt="" title=""><span id="p4" style="background:#c4c4c4;"></span>P &lt; 4</div>
+			<div class="p_legend" alt="" title=""><span id="4p8" style="background:#fa4f47;"></span>4 &lt; P &lt; 8</div>
+			<div class="p_legend" alt="" title=""><span id="8p15" style="background:#fb8a3d;"></span>8 &lt; P &lt; 15</div>
+			<div class="p_legend" alt="" title=""><span id="15p25" style="background:#91ff00;"></span>15 &lt; P &lt; 25</div>
+			<div class="p_legend" alt="" title=""><span id="25p50" style="background:#60feff;"></span>25 &lt; P &lt; 50</div>
+			<div class="p_legend" alt="" title=""><span id="p50" style="background:#4b44ff;"></span>P &gt; 50</div>
 		</div>
 
 	</div>
