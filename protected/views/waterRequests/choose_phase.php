45e59421c22ca10e748eec5979c0ccd4024bb528
Lorenzo Pini <lore.gnafu@gmail.com>
2013-05-24 13:23:54 +0200

    IE9 compat and link fix
    User was unable to create phase 3 children

--- protected/views/waterRequests/choose_phase.php
+++ protected/views/waterRequests/choose_phase.php
@@ -67,6 +67,7 @@ $this->menu=array(
 			Al termine del periodo la richiesta verr&agrave; automaticamente marcata come <b><i>Scaduta</i></b>.<br/><br/>
 		</p>
 	</div>
+</a>
 	<div id="parent_wr_phase_one" style="display:none;">
 		<?php
 			if ($dataProvider_phase_two) {
@@ -81,20 +82,20 @@ $this->menu=array(
 						'timestampHR',
 						array(
 							'name'=>'rounded_water_demand',
-							'value'=>'$data->rounded_water_demand." ".Yii::app()->params[\'water_demand_unit\']',
+							'value'=>'$data->rounded_water_demand." ".Yii::app()->params[\'water_demand_unit\']'
 						),
 						array(
 	     					'name'=>'',
 	     					'type'=>'raw',
-	     					'value' => 'CHtml::link(Yii::t(\'waterrequest\', \'select\'),Yii::app()->urlManager->createUrl("waterRequests/create",array("phase"=>2,"parent"=>$data->id)))',
-	    				),
-					),
+	     					'value' => 'CHtml::link(Yii::t(\'waterrequest\', \'select\'),Yii::app()->urlManager->createUrl("waterRequests/create",array("phase"=>2,"parent"=>$data->id)))'
+	    				)
+					)
 				));
-			}
+			};
 			echo CHtml::link(Yii::t('waterrequest', 'Create New Water Request'),CController::createUrl('waterRequests/create',array('phase'=>2)));		
 		?>
 	</div>
-</a>
+
 
 <br/>
 
@@ -119,35 +120,36 @@ $this->menu=array(
 			 Ogni transizione di stato genera una notifica/invio di una mail all'utente. 
 			 Oltre tutto, &egrave; possibile vedere in ogni istante l'iter seguito, con i relativi commenti e data.
 		</p>
-	
-		<div id="parent_wr_phase_two" style="display:none;">
-			<?php
-				if ($dataProvider_phase_three) {
-					
-					$this->widget('zii.widgets.grid.CGridView', array(
-						'id'=>'parent-wr2-grid',
-						'dataProvider'=>$dataProvider_phase_three,
-						
-						'columns'=>array(
-							'id',
-							'project',
-							'timestampHR',
-							array(
-								'name'=>'rounded_water_demand',
-								'value'=>'$data->rounded_water_demand." ".Yii::app()->params[\'water_demand_unit\']',
-							),
-							array(
-		     					'name'=>'',
-		     					'type'=>'raw',
-		     					'value' => 'CHtml::link(Yii::t(\'waterrequest\', \'select\'),Yii::app()->urlManager->createUrl("waterRequests/create",array("phase"=>2,"parent"=>$data->id)))',
-		    				),
-						),
-					));
-				}
-				echo CHtml::link(Yii::t('waterrequest', 'Create New Water Request'),CController::createUrl('waterRequests/create',array('phase'=>3)));		
-			?>
-		</div>
-	</div>
+	</div>	
 </a>
+	<div id="parent_wr_phase_two" style="display:none;">
+		<?php
+			if ($dataProvider_phase_three) {
+				
+				$this->widget('zii.widgets.grid.CGridView', array(
+					'id'=>'parent-wr2-grid',
+					'dataProvider'=>$dataProvider_phase_three,
+					
+					'columns'=>array(
+						'id',
+						'project',
+						'timestampHR',
+						array(
+							'name'=>'rounded_water_demand',
+							'value'=>'$data->rounded_water_demand." ".Yii::app()->params[\'water_demand_unit\']'
+						),
+						array(
+	     					'name'=>'',
+	     					'type'=>'raw',
+	     					'value' => 'CHtml::link(Yii::t(\'waterrequest\', \'select\'),Yii::app()->urlManager->createUrl("waterRequests/create",array("phase"=>3,"parent"=>$data->id)))'
+	    				)
+					)
+				));
+			};
+			echo CHtml::link(Yii::t('waterrequest', 'Create New Water Request'),CController::createUrl('waterRequests/create',array('phase'=>3)));		
+		?>
+	</div>
+
+
 
 
